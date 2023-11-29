0b806f7900e4736e367e0eff22126326362def7e
Lusine Aslanyan <lusine.aslanyan199425@gmail.com>
2023-11-29 07:11:40 -0800

    Add createNavBar function

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -8,3 +8,25 @@ function createHeader({ element, content }) {
     createNavBar({ element: "nav", content: "ul" })
   );
 }
+function createNavBar({ element, content }) {
+  const navBar = {
+    Home: "#header",
+    Products: "#products",
+    Contacts: "#contacts",
+    "About Us": "#aboutUs",
+  };
+  const nav = document.createElement(element);
+  nav.setAttribute("class", "list");
+  const ul = document.createElement(content);
+  ul.setAttribute("class", "navigation");
+  nav.appendChild(ul);
+  for (let key in navBar) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = navBar[key];
+    a.innerText = key;
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+  return nav;
+}
