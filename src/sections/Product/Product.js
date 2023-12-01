86dce5d366b6bb998a5c530c2da39ba36561f9d1
kazrias <ka6rias@gmail.com>
2023-12-01 16:16:46 +0400

    Add imgs

--- src/sections/Product/Product.js
+++ src/sections/Product/Product.js
@@ -1,4 +1,7 @@
-export const createProductSection = () => {
-  
+export const ProductList = () => {
+  const productList = document.createElement('div');
+  productList.classList.add('.product-list');
+  const imgs=['freshwaterShrimp','freshwaterFish','freshwaterSnails','freshwaterCrayFish','freshwaterPlants','nanoFish','freshwaterCrabs','AquariumParkFood'];
+  const titles = ['Freshwater Shrimp', 'Freshwater fish', 'Freshwater Snails', 'freshwater crayfish', 'freshwater plants', 'nano fish', 'freshwater crabs', 'aquarium park`s brand foods and supplies'];
 }
 
