711dfcd0f9374069d9514b3244e15c09b24765d6
kazrias <ka6rias@gmail.com>
2023-12-01 19:15:53 +0400

    Add product section and fonts

--- src/sections/Product/Product.js
+++ src/sections/Product/Product.js
@@ -1,18 +1,32 @@
-const imgs = ['freshwaterShrimp', 'freshwaterFish', 'freshwaterSnails', 'freshwaterCrayFish', 'freshwaterPlants', 'nanoFish', 'freshwaterCrabs', 'AquariumParkFood'];
-const titles = ['Freshwater Shrimp', 'Freshwater fish', 'Freshwater Snails', 'freshwater crayfish', 'freshwater plants', 'nano fish', 'freshwater crabs', 'aquarium park`s brand foods and supplies'];
+import './Product.css';
 const imgContext = require.context('../../img/', false, /\.(jpe?g|png|webp|gif|svg)$/i);
+const imgs = ['freshwaterSnails', 'freshwaterShrimp', 'freshwaterFish', 'freshwaterCrayFish', 'freshwaterPlants', 'nanoFish', 'freshwaterCrabs', 'AquariumParkFood'];
+const titles = ['Freshwater Snails', 'Freshwater Shrimp', 'Freshwater fish', 'freshwater crayfish', 'freshwater plants', 'nano fish', 'freshwater crabs', 'aquarium park`s brand foods and supplies'];
+
+
 
 export const ProductList = async () => {
   const section = document.querySelector('.product>.container');
   const productList = document.createElement('div');
   productList.classList.add('product-list');
-
+  const categories = document.createElement('h2')
+  categories.classList.add('product-categories');
+  categories.innerText = 'Popular Products';
+  section.append(categories);
   for (let i = 0; i < titles.length; i++) {
     const productItem = document.createElement('div');
     productItem.classList.add('product-list__item');
+
     const img = document.createElement('img');
+    img.classList.add('product-list__img');
     img.src = imgContext(`./${imgs[i]}.webp`);
-    productItem.append(img);
+
+    const title = document.createElement('h3');
+    title.classList.add('product-list__title');
+    title.innerText = `${titles[i]}`;
+
+
+    productItem.append(img, title);
     productList.append(productItem);
   }
   section.append(productList);
