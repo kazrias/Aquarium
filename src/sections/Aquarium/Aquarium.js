2baff20ab5116e46a503aec8d7caf661e6e87a27
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2023-12-02 13:53:32 +0400

    creat aquarium function

--- src/sections/Aquarium/Aquarium.js
+++ src/sections/Aquarium/Aquarium.js
@@ -1,5 +1,3 @@
-import './Aquarium.css'
-const imgContext=require.context('../../img/aquarium',false,/\.(jpe?g|png|webp|gif|svg|mp4)$/i)
 export const Aquarium = () => {
     const mainDiv = document.querySelector(".aquarium");
 
@@ -19,82 +17,4 @@ export const Aquarium = () => {
             </div>
     `;
 
-    const canvas = document.getElementById("canvas");
-    const context = canvas.getContext("2d");
-    const bntForBlueFish = document.getElementById("blueFish");
-    const bntForRedFish = document.getElementById("redFish");
-    const bntForOrangeFish = document.getElementById("orangeFish");
-
-    const date = {
-        xDelta: 100,
-        fishes: []
-    }
-
-    function update() {
-        date.fishes.forEach(function (fish) {
-            fish.update();
-        });
-    }
-    function draw() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        date.fishes.forEach(function (fish) {
-            fish.draw();
-        });
-    }
-
-    function loop() {
-        requestAnimationFrame(loop);
-        update();
-        draw();
-    }
-    loop();
-
-    function Fish(img, backImg) {
-        const fishImage = document.createElement("img");
-        fishImage.src = img;
-
-        this.x = Math.random() * 10 + 50;
-        this.y = Math.random() * 60 + 30;
-        this.width = 30;
-        this.height = 30;
-        this.xDelta = Math.random() * 4;
-        this.update = function () {
-            if (this.x + this.width > canvas.width) {
-                this.xDelta *= -1;
-                fishImage.src = backImg;
-            }
-            if (this.x < 0) {
-                this.xDelta *= -1;
-                fishImage.src = img;
-            }
-            this.x += this.xDelta;
-        },
-            this.draw = function () {
-                context.drawImage(fishImage, this.x, this.y, this.width, this.height);
-                context.beginPath();
-                context.fill();
-            }
-    }
-
-
-    bntForBlueFish.addEventListener("click", function () {
-        const img = `${imgContext('./BlueFish.png')}`;
-        const backImg = `${imgContext('./BlueBack.png')}`;
-        const fish = new Fish(img, backImg);
-        date.fishes.push(fish);
-    });
-
-    bntForRedFish.addEventListener("click", function () {
-        const img = `${imgContext('./RedFish.png')}`;
-        const backImg = `${imgContext('./RedBack.png')}`;
-        const fish = new Fish(img, backImg);
-        date.fishes.push(fish);
-    });
-
-    bntForOrangeFish.addEventListener("click", function () {
-        const img = `${imgContext('./OrangeFish.png')}`;
-        const backImg = `${imgContext('./OrangeBack.png')}`;
-        const fish = new Fish(img, backImg);
-        date.fishes.push(fish);
-    });
 }
